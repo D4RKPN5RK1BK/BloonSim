d3b25ee14f6cf64bd795622b6bab04abf3faa715
d4rk <d4rkpn5rk1bk@gmail.com>
2024-02-10 02:56:52 +0500

    Кнопки в главном меню работают

---
+++ Assets/Scripts/BubbleController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BubbleController : MonoBehaviour
+{
+    public float waveOffset = 0;
+
+    public float horizontalOffset = 5;
+
+    public float verticalOffset = 5;
+
+    private Vector3 initialPosition;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        initialPosition = rectTransform.localPosition;
+    }
+
+    private void Update()
+    {
+        var offset = Mathf.Sin(Time.time + rectTransform.position.x + waveOffset) * verticalOffset;
+        rectTransform.localPosition = initialPosition + new Vector3(0, offset, 0);
+    }
+}
